00c1a3d8dbc5bf295a5b8e0ad6dcda6519cbad54
Alchi404 <you@example.com>
2026-09-07 12:43:40 +0200

    Datenbank angelegt / tets container generation

---
+++ db.js
@@ -0,0 +1,14 @@
+let myImgs = [
+"./assets/img/Compsit_test2.jpg",
+"./assets/img/Duplication.jpg",
+"./assets/img/Eulenkatzeeee2_c.png",
+"./assets/img/Gondor_Polaris2.jpg",
+"./assets/img/Guts_polaris.jpg",
+"./assets/img/hasi.jpg",
+"./assets/img/Hornet_polaris2.jpg",
+"./assets/img/Link_comkposite.jpg",
+"./assets/img/Majoras_Maske_polaris2.jpg",
+"./assets/img/Neptun4.jpg",
+"./assets/img/Pyramid_head_Polaris2.jpg",
+"./assets/img/Rostock_Natur.png"
+]
--- skript.js
+++ skript.js
@@ -1,5 +1,16 @@
 const dialogRef = document.getElementById("full_img_dialog")
 
+function renderPictures(){
+    let imgContainer = document.getElementById('content')
+
+    for (let i = 0; i < myImgs.length; i++) {
+        imgContainer.innerHTML += /*html*/`
+           <div class="test"></div>
+        `
+    }
+}
+
+
 function openDialog() {
     dialogRef.showModal();
 }
