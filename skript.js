eac50ed5256609ed298a0c0b03a2f6e06b823077
Alchi404 <you@example.com>
2026-09-07 14:02:21 +0200

    Thumbnail per Javaskript erstellt

--- skript.js
+++ skript.js
@@ -1,20 +1,26 @@
 const dialogRef = document.getElementById("full_img_dialog")
 
-function renderPictures(){
-    let imgContainer = document.getElementById('content')
-
-    for (let i = 0; i < myImgs.length; i++) {
-        imgContainer.innerHTML += /*html*/`
-           <div class="test"></div>
-        `
-    }
-}
-
-
 function openDialog() {
     dialogRef.showModal();
 }
 
 function closeDialog(){
     dialogRef.close()
+}
+
+function renderPictures(){
+    let imgContainer = document.getElementById('content')
+
+    for (let i = 0; i < myImgs.length; i++) {
+        imgContainer.innerHTML += /*html*/`
+           <img id="img${i}" class="preview_img" onclick="openDialog()">
+        `
+    }
+}
+
+    function loadIMG() {
+       for (let i = 0; i < myImgs.length; i++) {
+        let img = document.getElementById(`img${i}`);
+        img.src = myImgs[i];
+       }
 }
